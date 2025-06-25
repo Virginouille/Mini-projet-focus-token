9aa82c98edab72c0295178b107c09e86bec7a65f
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-06-25 20:50:41 +0200

    ajout jwtauthfilter

--- src/main/java/com/exemple/exotoken/config/SecurityConfig.java
+++ src/main/java/com/exemple/exotoken/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.exemple.exotoken.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     /**Methode password encoder*/
@@ -13,6 +15,8 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /**Méthode qui met en place la Security Filter Chain*/
 
     //SecurityFilterChain
     //Authentication manager
---
+++ src/main/java/com/exemple/exotoken/security/JWTAuthFilter.java
@@ -0,0 +1,61 @@
+package com.exemple.exotoken.security;
+
+import com.exemple.exotoken.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JWTAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ") ) {
+           filterChain.doFilter(request, response);
+           return;
+        }
+        final String jwt = authHeader.substring(7);
+
+        final String username = jwtService.extractUsername(jwt);
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        }
+
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+        if (jwtService.isTokenValid(jwt, userDetails) {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    .userDetails,
+                    null,
+            userDetails.getAuthorities()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            filterChain.doFilter(request, response);
+        }
+
+    }
+
+
+}
